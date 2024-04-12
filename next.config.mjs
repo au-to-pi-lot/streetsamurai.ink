9ab0a26905bfa0963fcd447047618afc4638bf35
Ink Brownell <inkbrownell@gmail.com>
2024-04-12 15:38:52 -0500

    Enable MDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,21 @@
+import MDX from '@next/mdx'
+
+const withMDX = MDX()
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withMDX({
+    // Configure `pageExtensions` to include MDX files
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+
     webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
 
         // Enable raw imports for .glsl files
-        config.module.rules.push(
-            {
-                test: /\.glsl$/,
-                type: 'asset/source',
-            }
-        )
+        config.module.rules.push({
+            test: /\.glsl$/, type: 'asset/source',
+        })
 
         return config
     },
-};
+});
 
 export default nextConfig;
