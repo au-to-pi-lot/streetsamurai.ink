b5a7d7b83a8dd851d3a072fcc79ceb16f387da7e
Ink Brownell <inkbrownell@gmail.com>
2024-08-16 17:08:23 -0500

    feat: Rework hazard tape component for horizontal and vertical orientations

--- src/components/hazard-tape.tsx
+++ src/components/hazard-tape.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 export interface HazardTapeProps {
     className?: string;
+    orientation?: 'horizontal' | 'vertical';
 }
 
-const HazardTape: React.FC<HazardTapeProps> = ({ className = '' }) => {
+const HazardTape: React.FC<HazardTapeProps> = ({ className = '', orientation = 'horizontal' }) => {
+    const orientationClass = orientation === 'vertical' ? 'hazard-tape-vertical' : 'hazard-tape-horizontal';
+    
     return (
-        <div className={`hazard-tape ${className}`}>
+        <div className={`hazard-tape ${orientationClass} ${className}`}>
             <div className="hazard-tape-inner"></div>
         </div>
     );
