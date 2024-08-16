9e9cb979a08538c0c7969aa29a73ed2ab4b6a708
Ink Brownell <inkbrownell@gmail.com>
2024-08-16 15:23:15 -0500

    fix: improve header accessibility and semantics

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,10 +9,10 @@ const Header = ({...props}: HeaderProps): React.JSX.Element => (
     <header className="hero-container h-[8rem] w-full [transform-style:preserve-3d] flex items-center justify-between px-4">
         <h1 className={`${LibreBarcode128Text.variable} tracking-tighter whitespace-nowrap text-xl sm:text-3xl font-barcode font-extrabold hero glitch layers border-2 p-2`}
             data-text="Street Samurai Aug Clinic">
-            <Link href="/" className="">Street Samurai Aug Clinic</Link>
+            <Link href="/" aria-label="Home">Street Samurai Aug Clinic</Link>
         </h1>
 
-        <nav>
+        <nav aria-label="Main Navigation">
             {/* Add navigation items here */}
         </nav>
     </header>
