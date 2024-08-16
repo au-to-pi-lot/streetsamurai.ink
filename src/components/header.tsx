9302399b29948c3ebafce697eb082e114aeebfad
Ink Brownell <inkbrownell@gmail.com>
2024-08-16 15:51:15 -0500

    feat: Center the header content

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export type HeaderProps = {} & React.PropsWithChildren
 
 const Header = ({...props}: HeaderProps): React.JSX.Element => (
 
-    <header className="hero-container h-[8rem] w-full [transform-style:preserve-3d] flex items-center justify-between px-4">
+    <header className="hero-container h-[8rem] w-full [transform-style:preserve-3d] flex items-center justify-center px-4">
         <h1 className={`${LibreBarcode128Text.variable} tracking-tighter whitespace-nowrap text-xl sm:text-3xl font-barcode font-extrabold hero glitch layers border-2 p-2`}
             data-text="Street Samurai Aug Clinic">
             <Link href="/" aria-label="Home">Street Samurai Aug Clinic</Link>
