e7d98d06407272a3dba07d25d469dabc1b3a8991
Ink Brownell <inkbrownell@gmail.com>
2024-08-16 15:48:31 -0500

    feat: Add libre barcode 128 text font

---
+++ src/lib/fonts/libre-barcode-128-text.ts
@@ -0,0 +1,9 @@
+import localFont from "next/font/local";
+
+const LibreBarcode128Text = localFont({
+    src: "libre-barcode-128-text.ttf",
+    display: "swap",
+    variable: "--font-libre-barcode-128-text"
+});
+
+export default LibreBarcode128Text;
