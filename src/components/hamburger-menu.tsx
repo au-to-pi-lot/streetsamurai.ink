872073ba185b537dd7681ffc538009370d7979a1
Ink Brownell <inkbrownell@gmail.com>
2024-08-17 13:57:06 -0500

    refactor: Refactor inline SVG in HamburgerMenu to a separate file

--- src/components/hamburger-menu.tsx
+++ src/components/hamburger-menu.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Image from 'next/image';
 
 export type HamburgerMenuProps = {
   children: React.ReactNode;
@@ -18,20 +19,13 @@ const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ children }) => {
         className="text-white focus:outline-none"
         aria-label="Toggle menu"
       >
-        <svg
+        <Image
+          src="/hamburger-icon.svg"
+          width={24}
+          height={24}
+          alt="Menu"
           className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
+        />
       </button>
       {isOpen && (
         <div className="absolute right-0 mt-2 w-48 bg-black border border-gray-700 rounded-md shadow-lg py-1">
