660a0d4f277ff06e9da802a50c80d907fae150a8
Ink Brownell <inkbrownell@gmail.com>
2024-08-17 15:19:37 -0500

    feat: Add a prop to `HazardTape` to reverse the direction of movement

--- src/components/hazard-tape.tsx
+++ src/components/hazard-tape.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 export interface HazardTapeProps {
     className?: string;
     orientation?: 'horizontal' | 'vertical';
+    reverse?: boolean;
 }
 
-const HazardTape: React.FC<HazardTapeProps> = ({ className = '', orientation = 'horizontal' }) => {
+const HazardTape: React.FC<HazardTapeProps> = ({ className = '', orientation = 'horizontal', reverse = false }) => {
     const orientationClass = orientation === 'vertical' ? 'hazard-tape-vertical' : 'hazard-tape-horizontal';
+    const reverseClass = reverse ? 'hazard-tape-reverse' : '';
     
     return (
-        <div className={`hazard-tape ${orientationClass} ${className}`}>
+        <div className={`hazard-tape ${orientationClass} ${reverseClass} ${className}`}>
             <div className="hazard-tape-inner"></div>
         </div>
     );
