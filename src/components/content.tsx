2576962b81e30b472a428c54494e177895c40daa
Ink Brownell <inkbrownell@gmail.com>
2024-08-17 15:06:01 -0500

    feat: Add border and drop shadow to content area

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -10,7 +10,7 @@ const Content = ({children}: ContentProps): React.JSX.Element => (
     <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow flex justify-center">
-            <div className="w-full max-w-prose my-4 px-4 sm:px-6 md:px-8 py-8 relative">
+            <div className="w-full max-w-prose my-4 px-4 sm:px-6 md:px-8 py-8 relative border-2 filter drop-shadow-[0_1px_5px]">
                 <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md"></div>
                 <div className="relative z-10">
                     {children}
