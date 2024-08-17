90921707b1ae1a0c71491756aeba7353f2a8bc78
Ink Brownell <inkbrownell@gmail.com>
2024-08-17 16:01:04 -0500

    feat: Add hazard tape to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,8 @@ import HazardTape from "@/components/hazard-tape";
 export type FooterProps = {}
 
 const Footer = ({...props}: FooterProps): React.JSX.Element => (
-    <footer className="h-[8rem] flex flex-col">
-        <HazardTape orientation="horizontal" className="w-full h-8 mb-4" />
+    <footer className="h-[8rem] flex flex-col px-4">
+        <HazardTape orientation="horizontal" reverse={true} className="w-full h-8 mb-4" />
         <div className="flex-grow">
             {/* Add your footer content here */}
         </div>
