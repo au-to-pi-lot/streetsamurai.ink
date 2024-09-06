51fd6e576f05cc56f16749cdda51e7ccec8fef09
Ink Brownell <inkbrownell@gmail.com>
2024-09-06 13:20:38 -0500

    fix: Use children normally

--- src/components/hamburger-menu.tsx
+++ src/components/hamburger-menu.tsx
@@ -26,14 +26,7 @@ const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ children }) => {
       </button>
       {isOpen && (
         <div className="">
-          {React.Children.map(children, (child) => {
-            if (React.isValidElement(child)) {
-              return React.cloneElement(child, {
-                className: `${child.props.className || ''} `.trim()
-              });
-            }
-            return child;
-          })}
+          {children}
         </div>
       )}
     </div>
